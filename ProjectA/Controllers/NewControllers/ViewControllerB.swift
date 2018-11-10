a29aec83da392c46f75c23b824442c85b5fbdab4
Pawel <pawel.milczarek@nextisgreat.com>
2018-11-10 23:42:20 +0100

    initial commit

---
+++ ProjectA/Controllers/NewControllers/ViewControllerB.swift
@@ -0,0 +1,30 @@
+//
+//  ViewControllerA.swift
+//  ProjectA
+//
+//  Created by milczi on 10.11.2018.
+//  Copyright © 2018 Webriders. All rights reserved.
+//
+
+import UIKit
+
+class ViewControllerB: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ ProjectA/Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ProjectA
+//
+//  Created by milczi on 10.11.2018.
+//  Copyright © 2018 Webriders. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
